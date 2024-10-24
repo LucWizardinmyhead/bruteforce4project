c97a0dc2349313cfc34df63e0164158e6e5c5226
Nothing <lucasjon817@gmail.com>
2024-10-24 01:02:25 -0700

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -1,6 +1,6 @@
 from zipfile import ZipFile
 
-def attemp_extract(zf_handle, password):
+def attempt_extract(zf_handle, password):
     try:
         zf_handle.extractall(pwd=password)
         return True
@@ -8,17 +8,17 @@ def attemp_extract(zf_handle, password):
         return False
 
 def main():
-    print("[+] Beginning bruteforce ")
+    print("[+] Starting bruteforce attack ")
     with ZipFile('enc.zip') as zf:
         with open('rockyou.txt', 'rb') as f:
             for p in f:
                 password = p.strip()
-                if attemp_extract(zf, password):
-                    print("[+] Correct Password: %s" % password)
+                if attempt_extract(zf, password):
+                    print("[+] Correct Password Found: %s" % password)
                     exit(0)
                 else:
                     print("[-] Incorrect Password: %s" % password)
 
-                    print("[+] Password not found in list")
+    print("[+] Password not found in the list")
 if __name__ == "__main__":
     main()
